51b611bfa6b8e1f5ef8b1c1968dd6c1195b68e90
Shanky Jain <shanky.jain71@t-mobile.com>
2020-04-29 13:16:05 +0530

    Fixed Test cases

--- apps/stocks/src/app/app.component.spec.ts
+++ apps/stocks/src/app/app.component.spec.ts
@@ -1,30 +1,36 @@
-import { TestBed, async } from '@angular/core/testing';
+import { TestBed, ComponentFixture, async } from '@angular/core/testing';
+import { RouterTestingModule } from '@angular/router/testing'
 import { AppComponent } from './app.component';
 
 describe('AppComponent', () => {
+  let component: AppComponent;
+  let fixture: ComponentFixture<AppComponent>;
+  let app;
+
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [AppComponent]
+      declarations: [AppComponent],
+      imports: [RouterTestingModule]
     }).compileComponents();
   }));
 
+  beforeEach(() => {
+    fixture = TestBed.createComponent(AppComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+    app = fixture.debugElement.nativeElement;
+  });
+
   it('should create the app', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.debugElement.componentInstance;
-    expect(app).toBeTruthy();
+    expect(component).toBeTruthy();
   });
 
   it(`should have as title 'stocks'`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('stocks');
+    expect(component.title).toEqual('stocks');
   });
 
   it('should render title in a h1 tag', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain(
+    expect(app.querySelector('h1').textContent).toContain(
       'Welcome to stocks!'
     );
   });
--- libs/stocks/feature-shell/src/lib/stocks/stocks.component.spec.ts
+++ libs/stocks/feature-shell/src/lib/stocks/stocks.component.spec.ts
@@ -1,14 +1,39 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { StocksComponent } from './stocks.component';
+import {
+  MatFormFieldModule,
+  MatInputModule,
+  MatSelectModule
+} from '@angular/material';
+import { SharedUiChartModule } from '@coding-challenge/shared/ui/chart';
+import { ReactiveFormsModule, FormControl } from '@angular/forms';
+import { PriceQueryFacade } from '@coding-challenge/stocks/data-access-price-query';
+import { StoreModule } from '@ngrx/store';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { StocksConstant } from '../stocks.constant';
 
 describe('StocksComponent', () => {
   let component: StocksComponent;
   let fixture: ComponentFixture<StocksComponent>;
+  let priceQueryFacade: PriceQueryFacade;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ StocksComponent ]
+      declarations: [StocksComponent],
+      imports: [
+        ReactiveFormsModule,
+        MatFormFieldModule,
+        MatInputModule,
+        MatSelectModule,
+        MatFormFieldModule,
+        MatInputModule,
+        MatSelectModule,
+        SharedUiChartModule,
+        StoreModule.forRoot({}),
+        BrowserAnimationsModule
+      ],
+      providers: [PriceQueryFacade]
     })
     .compileComponents();
   }));
@@ -16,10 +41,23 @@ describe('StocksComponent', () => {
   beforeEach(() => {
     fixture = TestBed.createComponent(StocksComponent);
     component = fixture.componentInstance;
-    fixture.detectChanges();
+    priceQueryFacade = fixture.debugElement.injector.get(PriceQueryFacade);
   });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
+  describe('ngOnInit()', () => {
+    it('should create StocksComponent', () => {
+      expect(component).toBeTruthy();
+    });
+
+    it('should call fetchQuote() on change in values of stockPrickerForm form control without custom date', () => {
+
+      spyOn(priceQueryFacade, 'fetchQuote').and.stub();
+
+      component.ngOnInit();
+      component.stockPickerForm.setValue(StocksConstant.sampleFormValues);
+
+      setTimeout(() => expect(priceQueryFacade.fetchQuote).toHaveBeenCalledTimes(1), 3000)
+      expect(component.stockPickerForm.value).toEqual(StocksConstant.sampleFormValues);
+    });
   });
 });
