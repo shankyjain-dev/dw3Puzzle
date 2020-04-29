722056c2bd18c252b970b33fb67941b7d52261ab
Shanky Jain <shanky.jain71@t-mobile.com>
2020-04-29 17:35:32 +0530

    Code change

--- libs/stocks/feature-shell/src/lib/stocks/stocks.component.ts
+++ libs/stocks/feature-shell/src/lib/stocks/stocks.component.ts
@@ -1,47 +1,56 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { PriceQueryFacade } from '@coding-challenge/stocks/data-access-price-query';
-import { Subscription } from 'rxjs';
 import { StocksConstant } from '../stocks.constant';
+import { Subject } from 'rxjs';
+import { takeUntil } from 'rxjs/operators';
 
 @Component({
   selector: 'coding-challenge-stocks',
   templateUrl: './stocks.component.html',
   styleUrls: ['./stocks.component.css']
 })
-export class StocksComponent implements OnInit {
+export class StocksComponent implements OnInit, OnDestroy{
   stockPickerForm: FormGroup;
   symbol: string;
   period: string;
   maxDate: Date;
   startDate: Date;
   endDate: Date;
-  subscription: Subscription;
+  error: String;
 
   quotes$ = this.priceQuery.priceQueries$;
 
   formLabels = StocksConstant.formLabels;
 
+  destroySubject$: Subject<void> = new Subject();
+
   constructor(private fb: FormBuilder, private priceQuery: PriceQueryFacade) {}
     
-  ngOnInit() {
+  ngOnInit(): void {
     this.maxDate = new Date();
     this.stockPickerForm = this.fb.group({
       symbol: [null, Validators.required],
       startDate: [null, Validators.required],
       endDate: [null, Validators.required]
     });
-    //if date range is invalid startDate will be same as endDate.
-    this.subscription = this.stockPickerForm.valueChanges.subscribe(value => {
-      const { startDate, endDate } = value;
-      if (endDate != null && endDate < startDate){    
-        this.stockPickerForm.patchValue({startDate : new Date(endDate)});
-      }
-    })
+
+    this.stockPickerForm.valueChanges.pipe(
+      takeUntil(this.destroySubject$)
+    )
+      .subscribe(value => {
+        const { startDate, endDate } = value;
+        if (endDate != null && endDate < startDate){    
+          this.stockPickerForm.patchValue({startDate : new Date(endDate)});
+        }},
+        (error) => {
+          this.error = 'An error occurred:' + error;
+        });
   }
 
   ngOnDestroy(): void {
-    this.subscription.unsubscribe();
+    this.destroySubject$.next(); // destroySubject$ is emitted to kill all subscriptions.
+    this.destroySubject$.complete(); // notifies observers that destroySubject$ has completed
   }
 
   fetchQuote() {
